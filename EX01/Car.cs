cbd72e4c065c1e35e0b03b9ef4c3a2c0c1a368be
João Junior <junior.joao1305@gmail.com>
2025-03-31 22:23:05 -0300

    01

---
+++ EX01/Car.cs
@@ -0,0 +1,18 @@
+public class Car
+{
+    //atributos
+    public string marca = "";
+    public string modelo = "";
+    public int ano = 0;
+
+    //métodos
+    public void getMarca(){
+        Console.WriteLine(marca);
+    }
+    public void getModelo(){
+        Console.WriteLine(modelo);
+    }
+    public void getAno(){
+        Console.WriteLine(ano);
+    }
+}
---
+++ EX01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace EX01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car batMovel = new Car();
+
+        batMovel.marca = "Volkswagem";
+        batMovel.modelo = "Tiguan";
+        batMovel.ano = 2023;
+
+        batMovel.getMarca();
+        batMovel.getModelo();
+        batMovel.getAno();
+
+    }
+}
